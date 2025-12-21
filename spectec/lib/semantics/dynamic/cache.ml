6b0f3be1fb1c2e38a5da5bbf828f5350096e0710
KunJeong <kunjeong99@naver.com>
2025-12-22 00:58:11 +0900

    fix(cache): structural hash to match compare
    Changes the standard hash function to a fully structural hash that
    matches the structural comparison used for caching.
    This fixes a potential correctness issue as well as boosting IL
    interpreter speed by around 1.25x.

--- spectec/lib/semantics/dynamic/cache.ml
+++ spectec/lib/semantics/dynamic/cache.ml
@@ -9,7 +9,48 @@ module Entry = struct
     && List.compare (fun v_a v_b -> Il.Value.compare v_a v_b) values_a values_b
        = 0
 
-  let hash = Hashtbl.hash
+  (* Structural hash matching the structural equality in Il.Value.compare *)
+
+  (* Infix operator for combining hash values. *)
+
+  let ( +! ) h1 h2 = (h1 * 65599) + h2
+  let hash_atom (atom : Xl.Atom.t) : int = Hashtbl.hash atom
+
+  let hash_num (num : Xl.Num.t) : int =
+    match num with `Nat n -> 0 +! Bigint.hash n | `Int i -> 1 +! Bigint.hash i
+
+  let hash_mixop (mixop : Xl.Mixop.t) : int =
+    List.fold_left
+      (fun hash atoms ->
+        List.fold_left
+          (fun hash atom -> hash +! hash_atom atom.Common.Source.it)
+          hash atoms)
+      2 mixop
+
+  let rec hash_value (v : Il.Value.t) : int =
+    match v.it with
+    | BoolV b -> 0 +! Hashtbl.hash b
+    | NumV n -> 1 +! hash_num n
+    | TextV s -> 2 +! Hashtbl.hash s
+    | StructV fields ->
+        List.fold_left
+          (fun hash (atom, v) ->
+            hash +! (hash_atom atom.Common.Source.it +! hash_value v))
+          3 fields
+    | CaseV (mixop, values) ->
+        let base_hash = 4 +! hash_mixop mixop in
+        List.fold_left (fun hash v -> hash +! hash_value v) base_hash values
+    | TupleV values ->
+        List.fold_left (fun hash v -> hash +! hash_value v) 5 values
+    | OptV None -> 6
+    | OptV (Some v) -> 7 +! hash_value v
+    | ListV values ->
+        List.fold_left (fun hash v -> hash +! hash_value v) 8 values
+    | FuncV id -> 9 +! Hashtbl.hash id.Common.Source.it
+
+  let hash (id, values) =
+    let base_hash = Hashtbl.hash id in
+    List.fold_left (fun hash v -> hash +! hash_value v) base_hash values
 end
 
 (* LFU (with LRU tiebreak) cache over Entry keys *)
